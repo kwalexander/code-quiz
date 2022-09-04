821e94fb8ec436d61af03054d911aab21c52e570
Kaspar Alexander <kaspar.alexander@gmail.com>
2022-09-04 18:48:04 -0400

    HTML sections for dynamic content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,43 @@
-// define questions, options, and correct answer (array?)
+// define questions, options, and correct answer (array?). Source: MDN https://developer.mozilla.org/
+var questions = [
+    {
+        question: "What is a function?",
+        options: ["Reusable block of code", "Math equation", "Type of file", "Programming language"],
+        answer: "Reusable block of code"
+    },
+    {
+        question: "What does var mean?",
+        options: ["Varnish", "Variable", "Value at risk", "Varsity"],
+        answer: "Variable"
+    },
+    {
+        question: "How do you prefix comments in Javascript?",
+        options: ["//", "<!-- -->", "<comment>", "You can't"],
+        answer: "//"
+    },
+    {
+        question: "What percent of websites use Javascript?",
+        options: ["1%", "100%", "98%", "61%"],
+        answer: "98%"
+    },
+    {
+        question: "Which is a type of Javascript loop?:",
+        options: ["Circle", "Cycle", "Random", "For"],
+        answer: "For"
+    },
+    {
+        question: "What is one way to generate a random number?",
+        options: ["random(number)", "Math.random", "random.number", "randomNumber"],
+        answer: "Math.random"
+    },
+    {
+        question: "What is a method?",
+        options: ["String of data", "Function within an object", "Array of data", "Nested function"],
+        answer: "Function within an object"
+    },
+];
+
+// DOM element references
 
 // function to start a timer when user clicks start button
 
