4f7ce114c40f072499989416cdb6a8d9a71a3e62
Kaspar Alexander <kaspar.alexander@gmail.com>
2022-09-04 17:54:29 -0400

    JS outline

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// define questions, options, and correct answer (array?)
+
+// function to start a timer when user clicks start button
+
+
+// function to show the quiz
+
+
+// function to progress to the next question (if statement or for loop?)
+
+
+// function to check if answer is correct
+
+
+// function to end the game
+
+
+// function to store the high score (input initials, add to local storage, stringify JSON)
+
+
+// function to show the high score
+
+
+// event listeners for directing clicks
