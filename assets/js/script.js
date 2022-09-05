eb3a5c9075207dfca1c53441bb928ad9295a5cea
Kaspar Alexander <kaspar.alexander@gmail.com>
2022-09-05 15:03:51 -0400

    functions for starting quiz, running timer and several placeholders

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-
-// define questions, options, and correct answer (array?). Source: MDN https://developer.mozilla.org/
+// define questions, options, and correct answer. Source: MDN https://developer.mozilla.org/
 var questions = [
     {
         question: "What is a function?",
@@ -41,7 +40,7 @@ var questions = [
 // DOM element references
     // header elements
     var viewScoresEl = document.querySelector("#viewScores");
-    var timeLeftEl = document.querySelector("timeLeft");
+    var timeLeftEl = document.querySelector("#timeLeft");
 
     // opening view
     var openingEl = document.querySelector("#opening");
@@ -56,7 +55,7 @@ var questions = [
     var inputHighScoreEl = document.querySelector("#inputHighScore");
     var scoreEl = document.querySelector("#score");
     var initialsEl = document.querySelector("#initials");
-    var submitScoreBtnEl = document.querySelecgor("#submitScore");
+    var submitScoreBtnEl = document.querySelector("#submitScore");
 
     // high scores view
     var highScoresEl = document.querySelector("#highScores");
@@ -64,32 +63,72 @@ var questions = [
     var goBackBtnEl = document.querySelector("#goBack");
     var clearScoresBtnEl = document.querySelector("#clearScores");
 
-// time and score variables
+// time, score, and question variables
 var startTime = 60;
 var timeElapsed = 0;
+var currentQuestion = 0;
 
-// function to start a timer when user clicks start button, and countdown in seconds
-function startTimer () {
-    timeLeftEl.textContent = startTime
-    interval = setInterval(function() {
-        timeElapsed++;
-        timeLeftEl.textContent = timeGiven - timeElapsed;
-    })
+// function to start the quiz and timer when user clicks the start button
+function startQuiz() {
+    questionIndex = 0; // will use this to track which question the user is on
+    totalTime = 60
+    timeLeftEl.textContent = totalTime;
+    initialsEl.textContent = "";
+    
+    // once quiz starts, high the opening screen, input HS screen, and HS screens, but show quiz questions and time left
+    openingEl.style.display = "none"
+    quizEl.style.display = "block"
+    inputHighScoreEl.style.display = "none"
+    highScoresEl.style.display = "none"
+    timeLeftEl.style.display = "block"
 
+    // use a standard timer interval, decrementing from the starting total time
+    var startTimer = setInterval(function() {
+        totalTime--;
+        timeLeftEl.textContent = totalTime;
+        
+        // once time has decreased to 0, clear the interval and if there aren't any more question run the game over function
+        if(totalTime <=0) {
+            clearInterval(startTimer);
+            if (questionIndex <questions.length - 1) {
+                gameOver();
+            }
+        }
+    }, 1000); // decrease time by 1000ms decrements
+
+    nextQuestion();
 }
 
-
-// function to show the quiz
-
+// function to stop timer -- no longer need this?
+function stopTimer () {
+    clearInterval(interval);
+}
 
 // function to progress to the next question (if statement or for loop?)
-
+function nextQuestion() {
+    // get the question and 4 options from the previously defined array
+    question.textContent = questions[questionIndex].question;
+    option0.textContent = questions[questionIndex].options[0];
+    option1.textContent = questions[questionIndex].options[1];
+    option2.textContent = questions[questionIndex].options[2];
+    option3.textContent = questions[questionIndex].options[3];
+}
 
 // function to check if answer is correct
-
+//// if user's answer matches correct answer, display Correct!
+//// else decrement 5 second penalty
+//// then increment the question index
 
 // function to end the game
+function gameOver () {
+    openingEl.style.display = "none"
+    quizEl.style.display = "none"
+    inputHighScoreEl.style.display = "none"
+    highScoresEl.style.display = "none"
+    timeLeftEl.style.display = "display"
 
+    // add more here
+}
 
 // function to store the high score (input initials, add to local storage, stringify JSON)
 
@@ -98,3 +137,9 @@ function startTimer () {
 
 
 // event listeners for directing clicks
+startQuizBtnEl.addEventListener("click", startQuiz);
+
+option0.addEventListener("click", )
+option1.addEventListener("click", )
+option2.addEventListener("click", )
+option3.addEventListener("click", )
